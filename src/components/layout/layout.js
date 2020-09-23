92107922da3e43afb32a21b54c6824fddfd6a837
AMagicharry <tomsonewan@gmail.com>
2020-09-23 12:32:55 -0700

    removed fade

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -54,33 +54,31 @@ class Layout extends Component {
     if (isMobile) {
       console.log("mobile")
       return (
-        <Fade delay={1000}>
-          <div className="layout__mobile">
-            <img
-              src={sliding}
-              alt="Alexas sliding out of a tube"
-              className="layout__mobile__img"
-            />
-            <h3 className="layout__mobile__title"> VOICES + VOIDS </h3>
-            <p className="layout__mobile__description">
-              Voices + Voids features interactive vignettes best experienced on
-              a computer
+        <div className="layout__mobile">
+          <img
+            src={sliding}
+            alt="Alexas sliding out of a tube"
+            className="layout__mobile__img"
+          />
+          <h3 className="layout__mobile__title"> VOICES + VOIDS </h3>
+          <p className="layout__mobile__description">
+            Voices + Voids features interactive vignettes best experienced on a
+            computer
+          </p>
+          <div className="layout__mobile__email text-link__underline">
+            <p>
+              <a href="mailto: ?subject=Voices + Voids&body=https://amandayehh.github.io/voices-and-voids/">
+                Email myself the link
+              </a>
             </p>
-            <div className="layout__mobile__email text-link__underline">
-              <p>
-                <a href="mailto: ?subject=Voices + Voids&body=https://amandayehh.github.io/voices-and-voids/">
-                  Email myself the link
-                </a>
-              </p>
-            </div>
-            <div
-              className="layout__mobile__enter text-link__underline"
-              onClick={() => this.changeWidth()}
-            >
-              <p>Continue to site, but expect quirky layout bugs</p>
-            </div>
           </div>
-        </Fade>
+          <div
+            className="layout__mobile__enter text-link__underline"
+            onClick={() => this.changeWidth()}
+          >
+            <p>Continue to site, but expect quirky layout bugs</p>
+          </div>
+        </div>
       )
       //    return
     } else {
